51259f3fee1001594083da0ceccfa7875200ca3c
Paweł Wilczyński <pawel@livepeer.org>
2025-05-28 12:48:51 +0200

    Make the code more pythonic

--- Livepeer.py
+++ Livepeer.py
@@ -1,5 +1,11 @@
 #!/bin/python3
-from lib import Util, Contract, State
+import logging
+from lib import Contract, State
+from lib.Util import getPrivateKey, getChecksumAddr
+
+
+log = logging.getLogger(__name__)
+
 
 # This class initializes an Orchestrator object
 class Orchestrator:
@@ -7,43 +13,49 @@ class Orchestrator:
         # Orch details
         self.source_address = obj._source_address
         # Get private key
-        self.source_private_key = Util.getPrivateKey(obj._source_key, obj._source_password)
+        self.source_private_key = getPrivateKey(obj._source_key, obj._source_password)
         # If the password was set via file or environment var but failed to decrypt, exit
         if self.source_private_key == "":
-            Util.log("Fatal error: Unable to decrypt keystore file. Exiting...", 1)
+            log.error("Fatal error: Unable to decrypt keystore file. Exiting...")
             exit(1)
-        self.source_checksum_address = Util.getChecksumAddr(obj._source_address)
+        self.source_checksum_address = getChecksumAddr(obj._source_address)
         # Set target adresses
         self.target_address = obj._target_address
-        self.target_checksum_address = Util.getChecksumAddr(obj._target_address)
+        self.target_checksum_address = getChecksumAddr(obj._target_address)
+
 
 # For each configured keystore, create a Orchestrator object
 if len(State.KEYSTORE_CONFIGS) != 1:
-    Util.log("Only 1 Keystore Config is currently supported. Exiting...", 1)
+    log.error("Only 1 Keystore Config is currently supported. Exiting...")
     exit(1)
+
+
 State.orchestrator = Orchestrator(State.KEYSTORE_CONFIGS[0])
 
-### Main logic
+
 def withdraw_fees():
-    Util.log("### Withdrawing Fees ###", 1)
+    # Main logic
+    log.info("### Withdrawing Fees ###")
     pending_fees = Contract.pendingFees()
     if pending_fees < State.ETH_THRESHOLD:
-        Util.log("{0} has {1:.4f} ETH in pending fees < threshold of {2:.4f} ETH".format(State.orchestrator.source_address, pending_fees, State.ETH_THRESHOLD), 1)
+        log.info("{0} has {1:.4f} ETH in pending fees < threshold of {2:.4f} ETH".format(State.orchestrator.source_address, pending_fees, State.ETH_THRESHOLD))
     else:
-        Util.log("{0} has {1:.4f} in ETH pending fees > threshold of {2:.4f} ETH, withdrawing fees...".format(State.orchestrator.source_address, pending_fees, State.ETH_THRESHOLD), 1)
+        log.info("{0} has {1:.4f} in ETH pending fees > threshold of {2:.4f} ETH, withdrawing fees...".format(State.orchestrator.source_address, pending_fees, State.ETH_THRESHOLD))
         Contract.doWithdrawFees()
 
+
 def fund_deposit():
-    Util.log("### Funding Deposit ###", 1)
+    log.info("### Funding Deposit ###")
     balance = Contract.getEthBalance()
     # Transfer ETH to Receiver Gateway's Deposit if threshold is reached
     if balance < State.ETH_THRESHOLD:
-        Util.log("{0} has {1:.4f} ETH in their wallet < threshold of {2:.4f} ETH".format(State.orchestrator.source_address, balance, State.ETH_THRESHOLD), 1)
+        log.info("{0} has {1:.4f} ETH in their wallet < threshold of {2:.4f} ETH".format(State.orchestrator.source_address, balance, State.ETH_THRESHOLD))
     elif State.ETH_MINVAL > balance:
-        Util.log("Cannot send ETH, as the minimum value {0:.4f} ETH to leave behind is larger than the balance {1:.4f} ETH".format(State.ETH_MINVAL, balance), 1)
+        log.info("Cannot send ETH, as the minimum value {0:.4f} ETH to leave behind is larger than the balance {1:.4f} ETH".format(State.ETH_MINVAL, balance))
     else:
-        Util.log("{0} has {1:.4f} in ETH pending fees > threshold of {2:.4f} ETH, sending some to {3}...".format(State.orchestrator.source_address, balance, State.ETH_THRESHOLD, State.orchestrator.target_address), 2)
+        log.info("{0} has {1:.4f} in ETH pending fees > threshold of {2:.4f} ETH, sending some to {3}...".format(State.orchestrator.source_address, balance, State.ETH_THRESHOLD, State.orchestrator.target_address))
         Contract.doFundDeposit(float(balance) - State.ETH_MINVAL)
+
 
 withdraw_fees()
 fund_deposit()
--- lib/Contract.py
+++ lib/Contract.py
@@ -1,59 +1,67 @@
 # Any functions which requires reading/writing smart contracts gets dumped here
 # Also connects to the RPC provider and holds accessors to the smart contracts
-import web3 #< Currency conversions
-import sys #< To exit the program
-import json #< Parse JSON ABI file
-# Import our own libraries
-from lib import Util, State
+
+import web3  # Currency conversions
+import sys
+import json
+import logging
+from lib import State
+
+
+log = logging.getLogger(__name__)
+
 
 BONDING_CONTRACT_ADDR = '0x35Bcf3c30594191d53231E4FF333E8A770453e40'
 TICKET_BROKER_CONTRACT_ADDR = '0xa8bB618B1520E284046F3dFc448851A1Ff26e41B'
 
 
-### Define contracts
+# Define contracts
 
-
-"""
-@brief Returns a JSON object of ABI data
-@param path: absolute/relative path to an ABI file
-"""
 def getABI(path):
+    """
+    Returns a dict with ABI data
+
+    :param str path: absolute/relative path to an ABI file
+    """
     try:
         with open(path) as f:
             info_json = json.load(f)
             return info_json["abi"]
-    except Exception as e:
-        Util.log("Fatal error: Unable to extract ABI data: {0}".format(e), 1)
+    except Exception:
+        log.exception("Fatal error: Unable to extract ABI data")
         sys.exit(1)
 
+
 abi_bonding_manager = getABI(State.SIPHON_ROOT + "/contracts/BondingManager.json")
-abi_ticket_broker= getABI(State.SIPHON_ROOT + "/contracts/TicketBroker.json")
+abi_ticket_broker = getABI(State.SIPHON_ROOT + "/contracts/TicketBroker.json")
+
 # connect to L2 rpc provider
 provider = web3.HTTPProvider(State.L2_RPC_PROVIDER)
 w3 = web3.Web3(provider)
 assert w3.is_connected()
+
 # prepare contracts
 bonding_contract = w3.eth.contract(address=BONDING_CONTRACT_ADDR, abi=abi_bonding_manager)
 ticket_broker_contract = w3.eth.contract(address=TICKET_BROKER_CONTRACT_ADDR, abi=abi_ticket_broker)
 
 
-### Orchestrator ETH logic
+# Orchestrator ETH logic
 
-def pendingFees():
+def pendingFees() -> float:
     try:
         pending_wei = bonding_contract.functions.pendingFees(State.orchestrator.source_checksum_address, 99999).call()
-        pending_eth = web3.Web3.from_wei(pending_wei, 'ether')
-        return pending_eth
-    except Exception as e:
-        Util.log("Unable to get pending fees: '{0}'".format(e), 1)
+        return web3.Web3.from_wei(pending_wei, 'ether')
+    except Exception:
+        log.warning("Unable to get pending fees", exc_info=True)
         return 0.0
 
-def doWithdrawFees(toTarget):
+
+def doWithdrawFees():
     try:
         # We take a little bit off due to floating point inaccuracies causing tx's to fail
         transfer_amount = web3.Web3.to_wei(float(State.orchestrator.balance_ETH_pending) - 0.00001, 'ether')
         receiver_address = State.orchestrator.source_checksum_address
-        Util.log("Withdrawing {0} WEI to {1}".format(transfer_amount, State.orchestrator.source_address), 1)
+        log.info("Withdrawing %s WEI to %s", transfer_amount, State.orchestrator.source_address)
         # Build transaction info
         transaction_obj = bonding_contract.functions.withdrawFees(receiver_address, transfer_amount).build_transaction(
             {
@@ -66,26 +74,27 @@ def doWithdrawFees(toTarget):
         # Sign and initiate transaction
         signed_transaction = w3.eth.account.sign_transaction(transaction_obj, State.orchestrator.source_private_key)
         transaction_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
-        Util.log("Initiated transaction with hash {0}".format(transaction_hash.hex()), 2)
+        log.info("Initiated transaction with hash %s", transaction_hash.hex())
         # Wait for transaction to be confirmed
         w3.eth.wait_for_transaction_receipt(transaction_hash)
-        Util.log('Withdraw fees success.', 2)
-    except Exception as e:
-        Util.log("Unable to withdraw fees: '{0}'".format(e), 1)
+        log.info('Withdraw fees success.')
+    except Exception:
+        log.exception("Unable to withdraw fees")
 
 
-def getEthBalance():
+def getEthBalance() -> float:
     try:
         balance_wei = w3.eth.get_balance(State.orchestrator.source_checksum_address)
         balance_ETH = web3.Web3.from_wei(balance_wei, 'ether')
         return balance_ETH
-    except Exception as e:
-        Util.log("Unable to get ETH balance: '{0}'".format(e), 1)
+    except Exception:
+        log.exception("Unable to get ETH balance")
+
 
 def doFundDeposit(amount):
     try:
         receiver_address = State.orchestrator.target_checksum_address
-        Util.log("Sending deposit {0} WEI directly to receiver's deposit {1}".format(amount, State.orchestrator.target_address), 2)
+        log.info("Sending deposit %s WEI directly to receiver's deposit %s", amount, State.orchestrator.target_address)
         amount_wei = web3.Web3.to_wei(amount, 'ether')
         # Build transaction info
         transaction_obj = ticket_broker_contract.functions.fundDepositAndReserveFor(receiver_address, amount_wei, 0).build_transaction(
@@ -102,9 +111,9 @@ def doFundDeposit(amount):
         # Sign and initiate transaction
         signed_transaction = w3.eth.account.sign_transaction(transaction_obj, State.orchestrator.source_private_key)
         transaction_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
-        Util.log("Initiated transaction with hash {0}".format(transaction_hash.hex()), 2)
+        log.info("Initiated transaction with hash %s", transaction_hash.hex())
         # Wait for transaction to be confirmed
         w3.eth.wait_for_transaction_receipt(transaction_hash)
-        Util.log('Fund deposit success.', 2)
-    except Exception as e:
-        Util.log("Unable to fund deposit: '{0}'".format(e), 1)
+        log.info("Fund deposit success.")
+    except Exception:
+        log.exception("Unable to fund deposit")
